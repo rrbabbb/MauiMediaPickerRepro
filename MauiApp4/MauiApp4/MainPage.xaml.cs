d235942f4afe4b5b95e5402f2bfbede033a025d1
rrbabbb <davidnagler@pm.me>
2025-11-19 14:49:57 +0100

    added solution

---
+++ MauiApp4/MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiApp4
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    private async void OnOldApiClicked(object? sender, EventArgs e)
+    {
+      var photo = await MediaPicker.PickPhotoAsync();
+      if (photo is not null)
+      {
+        ImportedImage.Source = ImageSource.FromFile(photo.FullPath);
+      }
+    }
+
+    private async void OnNewApiClicked(object? sender, EventArgs e)
+    {
+      var photos = await MediaPicker.PickPhotosAsync();
+      if (photos is not null)
+      {
+        var photo = photos.FirstOrDefault();
+        if (photo is not null)
+        {
+          ImportedImage.Source = ImageSource.FromFile(photo.FullPath);
+        }
+      }
+    }
+  }
+}
